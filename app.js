2d244188d3b83f77031f8c3cdfa4ebd69b3f0576
SalukiSwift <puregoldcoffee@gmail.com>
2023-10-22 13:18:07 +0200

    added button and user input

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const sizeOfGrid = 16
 
 const container = document.querySelector(".container")
+const resetButton = document.querySelector("button")
 
 const createGrid = (amtOfGrids) => {
+
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("wrapper")
+    
     for (let i = 0; i < amtOfGrids; i++) {
         const row = document.createElement("div")
         row.classList.add("grid_row")
@@ -15,8 +20,21 @@ const createGrid = (amtOfGrids) => {
             })
             row.appendChild(gridBox)
         }
-        container.appendChild(row)
+        //container.appendChild(row)
+        wrapper.appendChild(row)
     }
-
+container.appendChild(wrapper)
 }
+
 createGrid(sizeOfGrid)
+resetButton.addEventListener("click", () => {
+    let userSize = Number(prompt("What dimensions do you want for the new grid?"))
+    while (userSize > 100) {
+ 
+        userSize = Number(prompt("grid size small than 100?"))
+    }
+    const wrapper = document.querySelector(".wrapper")
+    wrapper.remove()
+    createGrid(userSize)
+})
+//createGrid(sizeOfGrid)
