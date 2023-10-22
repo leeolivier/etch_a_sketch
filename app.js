9c012e787072e6a7505831615ee1683493f64e77
SalukiSwift <puregoldcoffee@gmail.com>
2023-10-22 11:42:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const sizeOfGrid = 16
+
+const container = document.querySelector(".container")
+
+const createGrid = (amtOfGrids) => {
+    for (let i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement("div")
+        row.classList.add("grid_row")
+
+        for (let j = 0; j <amtOfGrids; j++) {
+            const gridBox = document.createElement("div")
+            gridBox.classList.add("grid_box")
+            gridBox.addEventListener("mouseenter", () => {
+                gridBox.style.backgroundColor = "black"
+            })
+            row.appendChild(gridBox)
+        }
+        container.appendChild(row)
+    }
+
+}
+createGrid(sizeOfGrid)
